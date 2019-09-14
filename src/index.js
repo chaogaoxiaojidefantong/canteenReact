3a880797635c31dcc600410e25d6b7477558913a
ylw <759646095@qq.com>
2019-09-14 15:23:17 +0800

    hmr

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function component() {
   let element = document.createElement('div');
 	 var btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hellocc', 'webpack'], ' ');
+  element.innerHTML = _.join(['aHellossyyyccww', 'webpack'], ' ');
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
   element.appendChild(btn);
@@ -12,3 +12,10 @@ function component() {
 }
 
 document.body.appendChild(component());
+console.log(module.hot+'zz');
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+       console.log('xxxAccepting the updated printMe module!');
+       printMe();
+     })
+    }
