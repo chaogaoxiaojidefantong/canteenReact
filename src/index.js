b0cda0d43caeac08aef9c1879af19972bcb90dcc
ylw <759646095@qq.com>
2019-09-17 22:56:08 +0800

    fenli

--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ function component() {
 
   return element;
 }
-
+if (process.env.NODE_ENV !== 'production') {
+     console.log('Looks like we are in development mode!');
+   }
  //document.body.appendChild(component());
  let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
  document.body.appendChild(element);
