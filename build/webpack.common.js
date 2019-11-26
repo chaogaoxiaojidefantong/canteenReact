89506aae3735c8d881128dfae7935184b14c85c3
ylw <759646095@qq.com>
2019-11-27 00:03:13 +0800

    依赖添加111

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,15 +1,17 @@
  const path = require('path');
  const { CleanWebpackPlugin } = require("clean-webpack-plugin");
  const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+ function resolve(relatedPath) {
+  return path.join(__dirname, relatedPath)
+}
  module.exports = {
    entry: {
-     app: './src/index.js'
+     app: '../src/index.js'
    },
    plugins: [
     new CleanWebpackPlugin(),
      new HtmlWebpackPlugin({
-      template: './src/index.html',
+      // template: resolve('../src/index.html'),
        title: 'Production'
      })
    ],
